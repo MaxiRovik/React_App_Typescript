271b4bbf109347defccfcb1f8038fcfe62459e88
Max <maxirovik@gmail.com>
2021-10-11 16:18:23 +0300

    Types of drag and drop events and types  useRef (ref in input)

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,18 +1,45 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useRef, useState} from 'react';
 
 
 const EventsExample:FC = () => {
-    const [value, setValue] = useState<string>('')
+    const [value, setValue] = useState<string>('');
+    const [isDrag, setIsDrag] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null);
+
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
        setValue(e.target.value)
     }
     const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(value)
+    console.log(inputRef.current?.value)
     }
+    const dragHandler =(e: React.DragEvent<HTMLDivElement>) => {
+        console.log('Drag')
+    }
+
+    const dropHandler = (e:React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(false)
+        console.log('Drop')
+    }
+    const leaveHandler = (e:React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(false)
+    }
+    const dragWithPreventHandler = (e:React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDrag(true)
+    }
+
     return (
         <div>
-             <input value = {value}  onChange= {changeHandler} type = "text"/>
+             <input value = {value}  onChange= {changeHandler} type = "text" placeholder='Controlled'/>
+             <input ref = {inputRef} type = "text" placeholder='Not controlled'/>
             <button onClick = {clickHandler}> Click me! </button>
+            <div onDrag = {dragHandler} draggable style ={{width:200, height: 200, background: 'red'}}></div>
+            <div onDrop = {dropHandler}
+                 onDragLeave={leaveHandler}
+                 onDragOver = {dragWithPreventHandler}
+                 style ={{width:200, height: 200, background:isDrag ? 'red' : "blue", marginTop: 15}}></div>
         </div>
     );
 };
